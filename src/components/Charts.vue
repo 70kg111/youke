c18e7fe2e53bcf4194970a99053c6e724009ba3e
70kg111 <76104718@qq.com>
2020-03-08 13:56:44 +0800

    图表组件ChartsData.vue 结构

---
+++ src/components/Charts.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="chart" :style="{width:chartWidth,height:chartHeight}">
+
+    </div>
+</template>
+
+<script lang="ts">
+  import {Component, Prop, Provide, Vue} from 'vue-property-decorator';
+  import echarts from 'echarts';
+
+  @Component({
+    components: {}
+  })
+  export default class Charts extends Vue {
+    //图标类型 line/bar/pie
+    @Prop({type: String, default: 'line'}) readonly chartType !: string;
+    @Prop(Object) chartData !: string | null;
+
+    @Provide() chartWidth: string = '';
+    @Provide() chartHeight: string = '';
+
+    created() {
+      this.generatorWidthAndHeight();
+    }
+
+    //图表的宽度和高度
+    generatorWidthAndHeight() {
+      this.chartWidth = `${document.body.offsetWidth * 0.8}px`;
+      this.chartHeight = `${document.body.offsetHeight * 0.6}px`;
+    }
+  };
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
